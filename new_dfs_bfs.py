a9d38fb4bd67e1d227008d0181e20ccdf2c63216
chen-ming-shen <chenmingshen20080907@gmail.com>
2025-04-17 01:19:15 +0800

    Update new_dfs_bfs.py

--- new_dfs_bfs.py
+++ new_dfs_bfs.py
@@ -9,25 +9,24 @@ start=(0,0)
 end=(4,4)
 def dfs (maze,start,end):#深度
     #走这个行为的实现要在一个 while 里实现
-    start=list(start)
-    end=list(end)
-    
-    route=[]#这个是记录路径的
-    start_my=start#使用一个变量保存动态的位置
+    tmp=["路径"]#用来调试用的
+    route=[list(start)]#这个是记录路径的，先加了起始坐标
+    start_my=list(start)#使用一个变量保存动态的位置
     intersection=[]#记录遇到的路口
-    direction=[
-        [start_my[0],start_my[1]-1],
-        [start_my[0],start_my[1]+1],
-        [start_my[0]-1,start_my[1]],
-        [start_my[0]+1,start_my[1]]
-        ]#存储方向的变量
-    
     while True:
-        if start_my == end:
-            return route
-        for 
-    return intersection
-    pass
+        if start_my == list(end):
+            return route #如果循环的坐标对象等于终点坐标时返回所有路径
+        intersection_tmp=[]
+        for dx, dy in [(-1,0),(1,0),(0,-1),(0,1)]:#dx and dy 是xy的变化量
+            x,y = start_my
+            nx,ny =x+dx,y+dy
+            if 0<= nx< len(maze) and  0<= ny < len(maze[0]):#选择除墙壁外的位置
+                if maze[nx][ny]==0:
+                    intersection_tmp.append([nx,ny])#如果是可以走的路添加到临时变量里
+        start_my=[intersection_tmp[0][0],intersection_tmp[0][1]]#更新位置变量
+        route.append(start_my)
+        tmp.append(route)
+    return tmp
 def bfs (maze,start,end):#广度
     pass
 #输出路径怎么走和有几个走法
