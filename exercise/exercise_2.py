e2a5dbaaffa9579b92dc1d2e895f5f37a0e1bdee
chen-ming-shen <chenmingshen20080907@gmail.com>
2025-04-24 22:46:46 +0800

    Update exercise_2.py

--- exercise/exercise_2.py
+++ exercise/exercise_2.py
@@ -1,38 +1,4 @@
-
-"""
-maze = [
-  [0, 0, 1, 0],
-  [1, 0, 1, 0],
-  [1, 0, 0, 0],
-  [1, 1, 1, 0]
-]
-origin=(0,0)#开始
-end=(len (maze[0]),len(maze))#结束
-current_location=list(origin)#当前位置里加入起点
-inspect=set()#用来检查走过的地方
-use_stack=[[0,0],[0,0]]#用来模拟栈的列表
-intersection=[]#记录路口
-while True:
-    if use_stack:
-        if use_stack[-1]==end:#如果当前位置是终点时
-            print (length)#路径长度
-            print (route)#路径
-    x,y=use_stack.pop()
-    for nx ,ny in [[x,y-1],[x,y+1],[x-1,y],[x+1,y]]:
-        if 0<= nx <= end[0] and 0<= ny <= end[1] and (nx,ny ) not in inspect:
-            ins_x=maze[ny]
-            if ins_x[nx]==0:
-                current_location=nx,ny
-                inspect.add(tuple([nx,ny]))
-                intersection.append([nx,ny])
-                use_stack.append([nx,ny])
-            else:
-                break
-        else:
-            break
-print(use_stack)
-"""
-
+import time 
 #利用切片操作，实现一个trim()函数，去除字符串首尾的空格，注意不要调用str的strip()方法：
 
 def trim(s):
@@ -67,3 +33,43 @@ elif trim('    ') != '':
     print('6测试失败!')
 else:
     print('所有测试成功!')
+    
+    
+    
+    
+#汉诺塔的移动可以用递归函数非常简单地实现。
+#请编写move(n, a, b, c)函数，它接收参数n，表示3个柱子A、B、C中第1个柱子A的盘子数量，然后打印出把所有盘子从A借助B移动到C的方法，例如：
+### **汉诺塔问题描述**
+#- 有三根柱子，编号为 **A、B、C**。
+#- 初始时，在柱子 **A** 上有 **n 个大小不同的盘子**，盘子从上到下按 **从小到大** 的顺序叠放。
+#- 目标：把所有盘子从柱子 **A** 移动到柱子 **C**，并遵守以下规则：
+#  1. **每次只能移动一个盘子**。
+#  2. **移动时，大盘子不能放在小盘子上**。
+#  3. **可以利用柱子 B 作为辅助**。
+
+
+move_times=0
+n=3
+def move(n, a, b, c):
+    global move_times
+    move_times+=1
+    if n == 1:#当递归到的最底层时
+        #print(f"{a} --> {c}")#对象移动到目标位置
+        pass
+    else:
+        move(n-1,a,c,b)#依次把a移动到b里
+        #print(f"{a} --> {c}")#把a移动到c里
+        move(n-1,b,a,c)#然后打印出把所有盘子从b借助a移动到C的方法里
+# 期待输出:
+# A --> C
+# A --> B
+# C --> B
+# A --> C
+# B --> A
+# B --> C
+# A --> C
+start_time=time.perf_counter()
+move(n, 'A', 'B', 'C')
+end_time=time.perf_counter()
+
+print(f"当n={n}时，递归次数{move_times}次，也是移动步数{move_times}次\n用时{end_time - start_time:.10f}秒")
