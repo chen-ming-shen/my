cc38bfdf4676cbb38a4097266f28455598787698
chen-ming-shen <chenmingshen20080907@gmail.com>
2025-04-25 00:13:21 +0800

    Update exercise_2.py

--- exercise/exercise_2.py
+++ exercise/exercise_2.py
@@ -24,8 +24,7 @@ elif trim('  hello') != 'hello':
     print('2测试失败!')
 elif trim('  hello  ') != 'hello':
     print('3测试失败!')
-#elif trim('  hello  world  ') != 'hello  world':
-    #print(trim('  hello  world  '))
+elif trim('  hello  world  ') != 'hello  world':
     print('4测试失败!')
 elif trim('') != '':
     print('5测试失败!')
@@ -34,7 +33,7 @@ elif trim('    ') != '':
 else:
     print('所有测试成功!')
     
-    
+print(trim('  hello  world  '))
     
     
 #汉诺塔的移动可以用递归函数非常简单地实现。
@@ -73,3 +72,55 @@ move(n, 'A', 'B', 'C')
 end_time=time.perf_counter()
 
 print(f"当n={n}时，递归次数{move_times}次，也是移动步数{move_times}次\n用时{end_time - start_time:.10f}秒")
+
+
+
+
+
+#构造一个1, 3, 5, 7, ..., 99的列表，可以通过循环实现：
+print([tmp for tmp in range(1,100,2)])
+
+
+
+
+#请使用迭代查找一个list中最小和最大值，并返回一个tuple：
+def findMinAndMax(L):
+    min_l,max_l=None,None
+    if not L:
+        return (min_l,max_l)
+    else:
+        min_l,max_l=L[0],L[0]
+        for tmp in L:
+            if tmp<=min_l:
+                min_l=tmp
+            if tmp >= max_l:
+                max_l=tmp
+        return (min_l,max_l)
+# 测试
+if findMinAndMax([]) != (None, None):
+    print('1测试失败!')
+elif findMinAndMax([7]) != (7, 7):
+    print('2测试失败!',findMinAndMax([7]))
+elif findMinAndMax([7, 1]) != (1, 7):
+    print('3测试失败!',findMinAndMax([7, 1]))
+elif findMinAndMax([7, 1, 3, 9, 5]) != (1, 9):
+    print('4测试失败!',findMinAndMax([7, 1, 3, 9, 5]))
+else:
+    print('全部测试成功!')
+    
+    
+    
+x = 'abc'
+y = 123
+print(isinstance(x, str))
+print(isinstance(y, str))
+#修改列表生成式，通过添加if语句保证列表生成式能正确地执行：
+L1 = ['Hello', 'World', 18, 'Apple', None]
+L2 = [tmp.lower()  for tmp in L1 if isinstance(tmp ,str)]
+
+# 测试:
+print(L2)
+if L2 == ['hello', 'world', 'apple']:
+    print('测试通过!')
+else:
+    print('测试失败!')
